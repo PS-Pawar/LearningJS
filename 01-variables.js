6f004513068a4dfd499e141a4baf54e63ca394c1
Pavan Pawar <pawansp2005@egmail.com>
2026-03-19 21:21:20 +0530

    variables

---
+++ 01-variables.js
@@ -0,0 +1,22 @@
+//  Variables Practice
+
+let name = "Pavan";
+let age = 21;
+let isStudent = true;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+
+let city = "Aurangabad";
+let course = "JavaScript";
+
+console.log(city);
+console.log(course);
+
+
+
+// variable= a container used to store data
+//stirng = text 
+//bolean value = true or false
+//number =numeric value
